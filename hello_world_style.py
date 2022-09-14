577a1ffdbc964e91d5ee4f8becb0837bcfaf0972
Krostofer <christopher.di-bert@stu.ocsb.ca>
2022-09-14 21:41:56 +0000

    Created Hello, World! Program with style.

---
+++ hello_world_style.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#Created By: Chris Di Bert
+#Date: Sep. 14, 2022
+#This program prints 'Hello, World!' with proper style.
+
+
+def main():
+    print("Hello, World! With style!")
+    print("This line is part of a function.")
+
+#If the program is being run by a user, the above function will be executed.
+
+if __name__ == "__main__":
+    main()
+    
